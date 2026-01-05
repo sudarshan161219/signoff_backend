01c05f06647d80e59f1d3fd73c47e6f7f23af482
sudarshan161219 <godhustler90956@gmail.com>
2026-01-05 23:04:25 +0530

    refactor(prismaClient.ts) v3.0

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,3 +1,22 @@
+// import { PrismaClient } from "@prisma/client";
+
+// export const prisma = new PrismaClient();
+
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// FIX: Pass an empty object {} to satisfy the constructor check
+const prismaClientSingleton = () => {
+  return new PrismaClient({});
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
